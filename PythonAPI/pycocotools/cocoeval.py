b136689732f49f8806eb15cdf45c423d94f96bdf
ucLh <luchwork@mail.ru>
2021-07-07 10:54:44 +0300

    Added recall for 0.5 IoU measurment

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -456,7 +456,7 @@ class COCOeval:
             print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
             return mean_s
         def _summarizeDets():
-            stats = np.zeros((12,))
+            stats = np.zeros((13,))
             stats[0] = _summarize(1)
             stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
             stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
@@ -469,6 +469,7 @@ class COCOeval:
             stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
             stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
             stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[12] = _summarize(0, iouThr=.5, maxDets=self.params.maxDets[1])
             return stats
         def _summarizeKps():
             stats = np.zeros((10,))
